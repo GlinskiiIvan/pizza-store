fcc46d043224123ed7a65304981ea2668d18471b
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-07-31 13:23:40 +0600

    categories & sorting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import styles from './App.module.scss';
 import Header from "./components/Header/Header";
 import Categories from "./components/Categories/Categories";
+import Sorting from "./components/Sorting/Sorting";
 
 function App() {
   return (
     <div className={styles.container}>
       <Header />
-        <div>
+        <div className={styles.options}>
             <Categories />
+            <Sorting />
         </div>
     </div>
   );
--- src/components/Sorting/Sorting.jsx
+++ src/components/Sorting/Sorting.jsx
@@ -3,8 +3,23 @@ import styles from './Sorting.module.scss'
 
 const Sorting = (props) => {
     return (
-        <div>
-
+        <div className={styles.wrapper}>
+            <b>Сортировка по:</b>
+            <p>Названию &#708;</p>
+            <div className={`${styles.options} ${styles.active}`}>
+                <ul>
+                    <li className={styles.active}>Названию &#708;</li>
+                    <li>Названию &#709;</li>
+                    <li>Цене &#708;</li>
+                    <li>Цене &#709;</li>
+                </ul>
+            </div>
+            {/*<select>
+                <option>Названию &#708;</option>
+                <option>Названию &#709;</option>
+                <option>Цене &#708;</option>
+                <option>Цене &#709;</option>
+            </select>*/}
         </div>
     );
 };
