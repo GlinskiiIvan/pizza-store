4614cd3b88fe2c72de5ede67bd79ec1b93e6c791
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-08-02 15:24:57 +0600

    using map in sorting

--- src/components/Sorting/Sorting.jsx
+++ src/components/Sorting/Sorting.jsx
@@ -2,18 +2,28 @@ import React from 'react';
 import styles from './Sorting.module.scss'
 
 const Sorting = (props) => {
+    const sorting = [
+        'Названию (ASC)',
+        'Названию (DESC)',
+        'Цене (ASC)',
+        'Цене (DESC)',
+    ]
+
     return (
         <div className={styles.wrapper}>
             <b>Сортировка по:</b>
             <p>Названию &#708;</p>
-            {/*<div className={`${styles.options} ${styles.active}`}>
+            <div className={`${styles.options} ${styles.active}`}>
                 <ul>
-                    <li className={styles.active}>Названию &#708;</li>
+                    {
+                        sorting.map((item) => <li key={item} className={``} >{item}</li>)
+                    }
+                    {/*<li className={styles.active}>Названию &#708;</li>
                     <li>Названию &#709;</li>
                     <li>Цене &#708;</li>
-                    <li>Цене &#709;</li>
+                    <li>Цене &#709;</li>*/}
                 </ul>
-            </div>*/}
+            </div>
         </div>
     );
 };
