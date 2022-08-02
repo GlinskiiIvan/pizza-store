8e59cb23c82f478e8e2192f576a191d1ca6ad572
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-08-02 15:16:06 +0600

    adaptive categories & sorting

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import styles from './MainPage.module.scss'
+
 import Categories from "../../components/Categories/Categories";
 import Sorting from "../../components/Sorting/Sorting";
 import PizzaList from "../../components/PizzaList/PizzaList";
 
+import styles from './MainPage.module.scss'
+
 const MainPage = (props) => {
     return (
         <div>
-            {/*<div className={styles.options}>
+            <div className={styles.options}>
                 <Categories />
                 <Sorting />
-            </div>*/}
+            </div>
             <h2 className={styles.title}>Все пиццы</h2>
             <PizzaList />
         </div>
