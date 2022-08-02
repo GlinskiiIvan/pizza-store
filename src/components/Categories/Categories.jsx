c1804a5024c1140f56ca204b46f19916372121e5
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-08-02 15:33:29 +0600

    change category

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Categories.module.scss'
 
 const Categories = (props) => {
+    const [activeCategory, setActiveCategory] = useState(0);
+
     const categories = [
         'Все',
         'Мясные',
@@ -11,10 +13,22 @@ const Categories = (props) => {
         'Закрытые',
     ]
 
+    const setCategoryHandler = (index) => {
+        setActiveCategory(index);
+    }
+
     return (
         <ul className={styles.wrapper}>
             {
-                categories.map((item) => <li key={item} className={`${styles.item}`}>{item}</li>)
+                categories.map((item, index) => (
+                    <li
+                        key={item}
+                        className={`${styles.item} ${activeCategory == index ? styles.active : ''}`}
+                        onClick={() => setCategoryHandler(index)}
+                    >
+                        {item}
+                    </li>
+                ))
             }
         </ul>
     );
