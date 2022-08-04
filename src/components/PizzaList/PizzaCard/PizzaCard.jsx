d56c0388a2f5377424eafa15db3bf02401d9dfe4
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-08-04 19:16:44 +0600

    Изменение цены в зависимости от выбранного размера

--- src/components/PizzaList/PizzaCard/PizzaCard.jsx
+++ src/components/PizzaList/PizzaCard/PizzaCard.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './PizzaCard.module.scss'
 
 const PizzaCard = (props) => {
+    const [activeDough, setActiveDough] = useState(0);
+    const [activeSize, setActiveSize] = useState(0);
+
+    const changeDoughHandler = (index) => {
+        setActiveDough(index);
+    }
+    const changeSizeHandler = (index) => {
+        setActiveSize(index);
+    }
+
     return (
         <div className={styles.wrapper}>
-            <img src="./img/pizza-0.png" alt="pizza"/>
-            <h4>Чизбургер-пицца</h4>
+            <img src={props.data.imageUrl} alt="pizza"/>
+            <h4>{props.data.title}</h4>
             <div className={styles.options}>
                 <ul className={styles.dough}>
-                    <li className={styles.active}>тонкое</li>
-                    <li>традиционное</li>
+                    {
+                        props.data.types.map((item, index) => (
+                            <li
+                                className={`${activeDough == index ? styles.active : ''}`}
+                                onClick={() => changeDoughHandler(index)}
+                            >
+                                {item}
+                            </li>
+                        ))
+                    }
                 </ul>
                 <ul className={styles.size}>
-                    <li>26 см.</li>
-                    <li className={styles.active}>30 см.</li>
-                    <li>40 см.</li>
+                    {
+                        props.data.sizes.map((item, index) => (
+                            <li
+                                className={`${activeSize == index ? styles.active : ''}`}
+                                onClick={() => changeSizeHandler(index)}
+                            >
+                                {item} см.
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
             <div className={styles.buy}>
-                <span>1640 тг</span>
-                <button>+ Купить <span>2</span></button>
+                <span>{props.data.prices[activeSize]} тг</span>
+                <button>+ Купить <span>0</span></button>
             </div>
         </div>
     );
--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './PizzaList.module.scss'
 import PizzaCard from "./PizzaCard/PizzaCard";
 
 const PizzaList = (props) => {
+    const [pizzas, setPizzas] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://62e8efe1249bb1284eb6be90.mockapi.io/pizzas`)
+            .then((data) => data.json())
+            .then((json) => setPizzas(json))
+    }, [])
+
+    if(!pizzas) return;
+
     return (
         <div className={styles.wrapper}>
-            <PizzaCard />
-            <PizzaCard />
-            <PizzaCard />
-            <PizzaCard />
-            <PizzaCard />
-            <PizzaCard />
+            {
+                pizzas.map((pizza) => (
+                    <PizzaCard key={pizza.id} data={pizza} />
+                ))
+            }
         </div>
     );
 };
 
 export default PizzaList;
+
