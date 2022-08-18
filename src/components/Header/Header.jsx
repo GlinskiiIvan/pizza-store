831d4319a70cd3b18a4dac598441affd31b75454
Glinsky Ivan <glinsky_ivan@mail.ru>
2022-08-18 13:51:42 +0600

    Скрытие кнопки корзины при открытии корзины

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import styles from './Header.module.scss'
 import {useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import {Link, useLocation, useParams} from "react-router-dom";
 
 const Header = (props) => {
     const totalQuantity = useSelector(state => state.cart.totalQuantity);
     const totalPrice = useSelector(state => state.cart.totalPrice);
+    const location = useLocation();
 
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
-                <img src="./img/logo.svg" alt="logo pizza-store"/>
+                <Link to='/'>
+                    <img src="./img/logo.svg" alt="logo pizza-store"/>
+                </Link>
                 <div>
                     <h3>pizza store</h3>
                     <p>самая вкусная пицца во вселенной</p>
                 </div>
             </div>
-            <div className={styles.cart}>
-                <Link to='/cart'>
-                    <button>
-                        <span className={styles.cartPrice}>{totalPrice} тг</span>
-                        <span className={styles.cartCount}>
+            {
+                location.pathname !== '/cart' && (
+                    <div className={styles.cart}>
+                        <Link to='/cart'>
+                            <button>
+                                <span className={styles.cartPrice}>{totalPrice} тг</span>
+                                <span className={styles.cartCount}>
                         <img src="./img/cart-icon.svg" alt="cart"/>
-                            {totalQuantity}
+                                    {totalQuantity}
                     </span>
-                    </button>
-                </Link>
-            </div>
+                            </button>
+                        </Link>
+                    </div>
+                )
+            }
         </header>
     );
 };
